93a7b65c60663e83708f55f27da57e7a5cbb1d64
정희원 <jeongheewon@jeonghuiwon-ui-MacBookPro.local>
2020-06-20 22:14:49 +0900

    금액 변경 기능 추가

--- setting.py
+++ setting.py
@@ -1,28 +1,28 @@
 #API 정보 설정
 BASE_URL = 'https://api.naver.com'
-API_KEY = '01000000007ff99c3c70244df9f84ae69ec049ed8c6c1eac54731a9d8588bcb30db174d3dc'
-SECRET_KEY = 'AQAAAAB/+Zw8cCRN+fhK5p7ASe2Ms4p3GjtWqdqpWphSzCT3ZA=='
-CUSTOMER_ID = '576009'
+API_KEY = ''
+SECRET_KEY = ''
+CUSTOMER_ID = ''
 
 # format = PC, MO
 # adtype = 쇼핑검색, 파워링크, 통합검색
 # page = searching을 진행할 페이지 갯수
-format = "MO"
-adtype = "파워링크"
-page = 5
+format = ""
+adtype = ""
+page = 0
 
 # campaign, adgroup 이름설정
-campaign_name = "1. 검색광고_애니시트 - 주간"
-adgroup_name = "!!!!!_대표키워드"
+campaign_name = ""
+adgroup_name = ""
 
 # search_word = naver에서 검색할 단어
 # identify_word = 등수 식별을 위한 단어 (파워링크 = url, 쇼핑검색 = 쇼핑상품ID)
 # modify_word = 금액 변경을 위해 사용되는 단어 (파워링크 = 키워드, 쇼핑검색 = 쇼핑상품ID)
-search_word = "시트지"
-identify_word = "http://hdi1533.modoo.at/"
-modify_word = "시트지"
+search_word = ""
+identify_word = ""
+modify_word = ""
 
 # 프로세스 동작 설정 (단위 : amount_unit(원), time(분), rank(등))
-amount_unit = 10
-time = 10
-rank = 7
+amount_unit = 0
+time = 0
+rank = 0
